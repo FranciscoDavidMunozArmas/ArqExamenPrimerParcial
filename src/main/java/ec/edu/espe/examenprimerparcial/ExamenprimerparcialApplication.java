d7e9a5d8134d43c287c1c279a9e9a87879592367
Damoz1606 <panchitodmun@gmail.com>
2022-11-28 19:31:51 -0500

    Initial commit

---
+++ src/main/java/ec/edu/espe/examenprimerparcial/ExamenprimerparcialApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.examenprimerparcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenprimerparcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenprimerparcialApplication.class, args);
+	}
+
+}
